e9111c9d665ae2081bb84b21e3d1235cc68987ae
Aeon Amberwood <aeon.amberwood@icloud.com>
2026-07-26 14:17:14 +1000

    Update Person.cs

--- Assets/Person.cs
+++ Assets/Person.cs
@@ -11,6 +11,19 @@ using UnityEngine;
 // Demonstrate the script working in play mode
 // Point out that each Person component works independently from each other
 
+
+///// Homework / if there is extra time in the lesson
+
+/// We're going to simulate aging in a simple way.
+
+// 1. Declare a variable to track "days passed"
+// 2. In Update, increase the variable by 1
+// 3. If the days passed is greater than 365...
+    // Increase age by 1
+    // Set the days passed to 0 (to start a new year)
+
+// When you press play, a person should automatically get older, and become an adult when reaching 18.
+
 public class Person : MonoBehaviour
 {
     // The person's first name
@@ -36,17 +49,21 @@ public class Person : MonoBehaviour
         CheckAge();
     }
 
+    ////// TEACHER! Challenge the learners to write this method themselves
+    /// Write only the comments and give them 5 minutes to write the code on their own
+    /// Offer to give help if they need it
+    /// Encourage them to try typing even if they aren't sure
     void CheckAge()
     {
         // If our age is larger or equal to 18...
         if (age >= 18)
         {
-            // Flag that we're an adult
+            // Set isAdult to true
             isAdult = true;
         }
         else // Otherwise...
         {
-            // Flag that we're not an adult
+            // Set isAdult to false
             isAdult = false;
         }
     }
